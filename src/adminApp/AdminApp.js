2917abaacd893f1b023ff608ca2bdd1ab03f78cc
mikkop <none@noemail>
2022-10-19 14:53:10 +0300

    Moved event handling back to main component

--- src/adminApp/AdminApp.js
+++ src/adminApp/AdminApp.js
@@ -1,8 +1,13 @@
 
+import { useReducer } from 'react';
+
 import '../App.css';
 
 import EditExam from './EditExam';
 import ExamMenu from '../ExamMenu';
+
+const answerStub = {answer: 'Vastaus', isCorrect: false};
+const questionStub = {question: 'Kysymys?', answers: [{...answerStub}]}
 
 const answers1 = [
   {answer: "Vastaus_1_1", isCorrect: true},
@@ -31,11 +36,51 @@ let exams = [exam1, exam2];
 
 const AdminApp = () => 
 {
-  
+  const [exam, dispatch] = useReducer(reducer, exams[0]);
+
+  function reducer(state, action)
+  {
+    let stateCopy = {...state};
+    
+    switch (action.type) {
+      case 'ANSWER_VALUE_CHANGED':
+        stateCopy.questions[action.payload.questionIndex].answers[action.payload.answerIndex].answer =
+          action.payload.value;
+        return stateCopy;
+      case 'ANSWER_CHECKED_STATE_CHANGED':
+        stateCopy.questions[action.payload.questionIndex].answers[action.payload.answerIndex].isCorrect =
+          action.payload.value;
+        return stateCopy;
+      case 'ADD_ANSWER_CLICKED':
+        //const answersCopy = stateCopy.question[action.payload.questionIndex].answers.map( answer =>
+        //console.log("add answer");
+        //stateCopy.questions[action.payload.questionIndex].answers.push({...answerStub});
+        //Shallow copy doesn't work properly, adds two answers:
+        /*stateCopy.questions = stateCopy.questions.slice();
+        stateCopy.questions[action.payload.questionIndex].answers = 
+          stateCopy.questions[action.payload.questionIndex].answers.slice();
+        stateCopy.questions[action.payload.questionIndex].answers.push({...answerStub});
+        return stateCopy;*/
+        //TODO: make a deep copy manually without JSON
+        stateCopy = JSON.parse(JSON.stringify(state));
+        stateCopy.questions[action.payload.questionIndex].answers.push({...answerStub});
+        return stateCopy;
+      case 'ADD_QUESTION_CLICKED':
+        stateCopy.questions = stateCopy.questions.slice();
+        stateCopy.questions.push({...questionStub});
+        return stateCopy;
+      case 'QUESTION_VALUE_CHANGED':
+        stateCopy.questions[action.payload.questionIndex].question = action.payload.value;
+        return stateCopy;
+      default:
+        throw Error('Unknown event: ' + action.type);
+    }
+  }
+
   return (
     <div className='App'>
-      {<ExamMenu exams={exams}/>}
-      {<EditExam exam={exams[0]} />}
+      {<ExamMenu exams={exams} dispatch={dispatch}/>}
+      {<EditExam exam={exam} dispatch={dispatch}/>}
     </div>
     )
 }
--- src/adminApp/EditExam.js
+++ src/adminApp/EditExam.js
@@ -1,70 +1,25 @@
 
-import { useReducer } from 'react';
 import '../App.css';
 import EditQuestion from './EditQuestion';
 
-const answerStub = {answer: 'Vastaus', isCorrect: false};
-const questionStub = {question: 'Kysymys?', answers: [{...answerStub}]}
-
 const EditExam = (props) => 
 {
-    const [exam, dispatch] = useReducer(reducer, props.exam);
-
-    function reducer(state, action)
-    {
-      let stateCopy = {...state};
-      
-      switch (action.type) {
-        case 'ANSWER_VALUE_CHANGED':
-          stateCopy.questions[action.payload.questionIndex].answers[action.payload.answerIndex].answer =
-            action.payload.value;
-          return stateCopy;
-        case 'ANSWER_CHECKED_STATE_CHANGED':
-          stateCopy.questions[action.payload.questionIndex].answers[action.payload.answerIndex].isCorrect =
-            action.payload.value;
-          return stateCopy;
-        case 'ADD_ANSWER_CLICKED':
-          //const answersCopy = stateCopy.question[action.payload.questionIndex].answers.map( answer =>
-          //console.log("add answer");
-          //stateCopy.questions[action.payload.questionIndex].answers.push({...answerStub});
-          //Shallow copy doesn't work properly, adds two answers:
-          /*stateCopy.questions = stateCopy.questions.slice();
-          stateCopy.questions[action.payload.questionIndex].answers = 
-            stateCopy.questions[action.payload.questionIndex].answers.slice();
-          stateCopy.questions[action.payload.questionIndex].answers.push({...answerStub});
-          return stateCopy;*/
-          //TODO: make a deep copy manually without JSON
-          stateCopy = JSON.parse(JSON.stringify(state));
-          stateCopy.questions[action.payload.questionIndex].answers.push({...answerStub});
-          return stateCopy;
-        case 'ADD_QUESTION_CLICKED':
-          stateCopy.questions = stateCopy.questions.slice();
-          stateCopy.questions.push({...questionStub});
-          return stateCopy;
-        case 'QUESTION_VALUE_CHANGED':
-          stateCopy.questions[action.payload.questionIndex].question = action.payload.value;
-          return stateCopy;
-        default:
-          throw Error('Unknown event: ' + action.type);
-      }
-    }    
-    
     return (
         <div className='exam'>
-            <h3>{exam.name}</h3>
+            <h3>{props.exam.name}</h3>
             <div className='kysymys-lista'>
-                {exam.questions.map( (question, index) => {
+                {props.exam.questions.map( (question, index) => {
                     return (<EditQuestion 
                         key={index}
                         question={question}
                         questionIndex={index}
-                        dispatch={dispatch}
+                        dispatch={props.dispatch}
                     /> )})
                 }
             </div>
             <div>
                 <input type='button' value='+'
-                    onClick={event => dispatch(
+                    onClick={event => props.dispatch(
                         {type: 'ADD_QUESTION_CLICKED',
                         payload: {}}
                     )}
